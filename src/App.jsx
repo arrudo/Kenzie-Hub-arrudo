9527d146975df97ea79619822f2a94fc2b9f5f7a
André <andrearruda068@gmail.com>
2023-08-26 08:57:48 -0300

    fix: loading page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { RoutesMain } from "./routes/RoutesMain";
 import "./styles/index.scss";
 import { useContext } from "react";
 import { UserContext } from "./providers/UserContext";
-import { LoadingWheel } from "./components/LoadingWheel";
 function App() {
   const { loading } = useContext(UserContext);
   return (
     <>
-      {loading ? <LoadingWheel /> : <RoutesMain />}
+      {loading ? (
+        <h1 className="loading_heading">Carregando...</h1>
+      ) : (
+        <RoutesMain />
+      )}
 
       <Toaster position="top-right" reverseOrder={false} />
     </>
