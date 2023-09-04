d7b20ccf145254b80edcbd8d1f0d5873f2b25313
André <andrearruda068@gmail.com>
2023-09-04 09:48:03 -0300

    readonly

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,19 @@
 import { forwardRef } from "react";
 import styles from "./style.module.scss";
-export const Input = forwardRef(
-  ({ id, label, error, readonly, ...rest }, ref) => {
-    return (
-      <div className={styles.input__container}>
-        <label className={styles.input__label} htmlFor={id}>
-          {label}
-        </label>
-        <input id={id} ref={ref} {...rest} readOnly={readonly ? true : false} />
-        <p className={styles.input__error}>{error ? error.message : null}</p>
-      </div>
-    );
-  }
-);
+
+export const Input = forwardRef(({ id, label, error, ...rest }, ref) => {
+  return (
+    <div className={styles.input__container}>
+      <label className={styles.input__label} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        id={id}
+        ref={ref}
+        {...rest}
+        readOnly={rest.readOnly ? true : false}
+      />
+      <p className={styles.input__error}>{error ? error.message : null}</p>
+    </div>
+  );
+});
